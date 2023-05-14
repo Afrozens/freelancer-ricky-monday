d4de9fdd7fb6d0b11ce191b2d5a65ee5c7bf20ce
Afrozens <jesuszen2.6@gmail.com>
2023-05-14 13:08:28 -0400

    upload: finish handle of data for db

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -3,25 +3,40 @@ import { ButtonTheme, PreviewImg } from '../../components'
 import { useData } from '../../contexts/ApiDataContext'
 import { saveInLocalStorage } from '../../utils'
 import { Buttoncross, Buttonlike, CrossIcon, Homepage } from './styles'
+import { DbData } from '../../interfaces'
+import { useAuth } from '../../contexts/AuthContext'
 
 const HomePage = () => {
-  const [dbData, setDbData] = useState({})
+  const [dbData, setDbData] = useState<DbData | null>(null)
+
+  const { user } = useAuth()
   const { dataApi, figure, setFigure } = useData()
   const { image, name } = dataApi
 
-  const handleLike = () => {
+  const handleInteractivity = (isLike: boolean) => {
     const adittion = figure + 1
     setFigure(adittion)
     saveInLocalStorage('figure', adittion)
+
+    const data: DbData = {
+      name: name,
+      image: image,
+      isLike: isLike,
+      uid: user?.uid as string,
+    }
+    setDbData(data)
+
+    console.log(dbData)
   }
+  
   return (
     <Homepage>
       <ButtonTheme />
       <PreviewImg image={image} name={name} />
-      <Buttoncross>
+      <Buttoncross onClick={() => handleInteractivity(false)}>
         <CrossIcon />
       </Buttoncross>
-      <Buttonlike onClick={handleLike}>
+      <Buttonlike onClick={() => handleInteractivity(true)}>
         <img src='/icons/like.svg' alt='like' />
       </Buttonlike>
     </Homepage>
